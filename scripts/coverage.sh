00eb56e65f53df9286bc6499f06342124367ee13
Reza <resal81@gmail.com>
2015-05-02 14:12:25 -0400

    removed echo line

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,7 +4,7 @@ set -e
 
 # http://stackoverflow.com/a/21142256/2055281
 
-echo "mode: atomic" > coverage.txt
+# echo "mode: atomic" > coverage.txt
 
 for d in $(find ./* -maxdepth 10 -type d); do
     if ls $d/*.go &> /dev/null; then
